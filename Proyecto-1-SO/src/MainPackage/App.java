059e73f0375093c66f6e9ff91ec2314ab7dfc831
angelgm1729 <angel.granado@correo.unimet.edu.ve>
2024-01-21 11:21:59 -0400

    first commit

---
+++ Proyecto-1-SO/src/MainPackage/App.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MainPackage;
+import GUI.Classes.Home;
+
+/**
+ *
+ * @author angel
+ */
+public class App {
+    
+    
+   public static void start(){
+//       String param = readTxt();
+//       loadParam(param);
+       Home home = new Home();
+       home.setVisible(true);
+   }
+    
+}
---
+++ Proyecto-1-SO/src/MainPackage/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package MainPackage;
+
+/**
+ *
+ * @author angel
+ */
+public class Main {
+/**
+     * main method
+     * @param args 
+     */
+    public static void main(String[] args){
+       App.start();
+    }
+    
+}
