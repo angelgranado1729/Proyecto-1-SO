10be607d587c48fa684f16d9d41410011dabdcde
angelgm1729 <angel.granado@correo.unimet.edu.ve>
2024-02-08 17:10:55 -0400

    fixing bugs

--- Proyecto-1-SO/src/MainClasses/ProjectManager.java
+++ Proyecto-1-SO/src/MainClasses/ProjectManager.java
@@ -9,6 +9,8 @@ import java.util.concurrent.Semaphore;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import Helpers.HelpersFunctions;
+
 /**
  *
  * @author Erika A. Henández Z.
@@ -18,7 +20,8 @@ public class ProjectManager extends Employee {
     private String currentState;
     private int strikes;
 
-    public ProjectManager(int company, int workerId, int type, int daysToFinish, int numOfWorkDone, int hourlyWage, Drive driveRef, Semaphore mutex) {
+    public ProjectManager(int company, int workerId, int type, int daysToFinish, int numOfWorkDone, int hourlyWage,
+            Drive driveRef, Semaphore mutex) {
         super(company, workerId, type, daysToFinish, numOfWorkDone, hourlyWage, driveRef, mutex);
         this.currentState = "Inactivo";
         this.strikes = 0;
@@ -28,14 +31,15 @@ public class ProjectManager extends Employee {
     public void run() {
         while (true) {
             try {
-                //Se obtiene la duración del día total directo de la variable de app.
+                // Se obtiene la duración del día total directo de la variable de app.
                 int dayDuration = App.getDayDuration();
 
                 // Duración de una hora en la simulación
                 int oneHour = dayDuration / 24;
 
                 // Se sabe que pasa 16 horas alterando ver anime y trabajar cada 30 mins.
-                // Por lo tanto se loopea cada 30 mins (32 intervalos de 30 mins) para las primeras 16 h. 
+                // Por lo tanto se loopea cada 30 mins (32 intervalos de 30 mins) para las
+                // primeras 16 h.
                 for (int i = 0; i < 32; i++) {
                     if (i % 2 == 0) {
                         this.setCurrentState("Viendo Anime");
@@ -55,12 +59,14 @@ public class ProjectManager extends Employee {
                 // Culminado el día cobra su salario
                 this.getPaid();
 
-                if (this.company == 0) {
-                    app.getNickelodeon().setTotalDays(app.getNickelodeon().getTotalDays() + 1);
-                } else {
-                    app.getCartoonNetwork().setTotalDays(app.getCartoonNetwork().getTotalDays() + 1);
-                }
+                this.getMutex().acquire();
+
+                this.updateDeadlineCountdown();
                 this.updateCountdown();
+                HelpersFunctions.calculateTotalCost(this.company, this.accumulatedSalary);
+                this.setAccumulatedSalary(0);
+
+                this.getMutex().release();
 
             } catch (InterruptedException ex) {
                 Logger.getLogger(ProjectManager.class.getName()).log(Level.SEVERE, null, ex);
@@ -68,7 +74,7 @@ public class ProjectManager extends Employee {
         }
     }
 
-    private void updateCountdown() {
+    private void updateDeadlineCountdown() {
         // Lógica para actualizar el contador de días restantes
         if (this.company == 0) {
             if (app.getNickelodeon().getRemainingDays() != 0) {
@@ -81,16 +87,18 @@ public class ProjectManager extends Employee {
         }
     }
 
-    private void getPaid() {
-        // Asumiendo que el Project Manager trabaja las 24 horas del día, incluyendo ver anime.
-        this.setAccumulatedSalary(this.getAccumulatedSalary() + this.getHourlyWage() * 24);
+    private void updateCountdown() {
         if (this.company == 0) {
-            app.getNickelodeon().increaseTotalCost(this.getHourlyWage() * 24);
-            app.getNickelodeon().setProfit(app.getNickelodeon().getProfit() - (this.hourlyWage * 24));
+            app.getNickelodeon().setTotalDays(app.getNickelodeon().getTotalDays() + 1);
         } else {
-            app.getCartoonNetwork().increaseTotalCost(this.getHourlyWage() * 24);
-            app.getCartoonNetwork().setProfit(app.getCartoonNetwork().getProfit() - (this.hourlyWage * 24));
+            app.getCartoonNetwork().setTotalDays(app.getCartoonNetwork().getTotalDays() + 1);
         }
+    }
+
+    private void getPaid() {
+        // Asumiendo que el Project Manager trabaja las 24 horas del día, incluyendo ver
+        // anime.
+        this.setAccumulatedSalary(this.getAccumulatedSalary() + this.getHourlyWage() * 24);
     }
 
     @Override
